e5ecf015401930023da3f317a7821ae7b9ff1f25
pranayjasrasaria <pranayjasrasaria@gmail.com>
2026-07-02 13:03:39 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+a=10
+b=12
+c=0
+
+if a and b and c:
+    print("All the numbers that have boolean values as true")
+else:
+    print("Atleast one value having boolean value as true")
+
+
+
+a=10
+b=-10
+c=0
+
+if a>0 or b>0:
+    print("Either of the number are greater than 0")
+else:
+    print("No number is greater than 0")
